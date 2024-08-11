3611abd3b8ddba74da345ef4238505339f151259
Itumeleng-Koalane <koalane567@gmail.com>
2024-08-11 20:25:18 +0200

    Made  changes for the email field to be optional.

--- VoteAppAPI/Models/Domain Model/Register.cs
+++ VoteAppAPI/Models/Domain Model/Register.cs
@@ -8,7 +8,7 @@ namespace VoteAppAPI.Domain_Model
         public Guid Id { get; set; }
         [MaxLength(13)]
         public string Idnumber { get; set; }
-        public string Email { get; set; }
+        public string? Email { get; set; }
         public string Name { get; set; }
         public string Surname { get; set; }
         public DateTime CreatedDate { get; set; } = DateTime.Now;
